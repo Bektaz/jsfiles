318e2696fa9b3eba6ffa0087a1fb239bae46e04d
Bereket <berekettaz@gmail.com>
2015-09-14 18:16:39 +0500

    first commit

---
+++ guessgifts.js
@@ -0,0 +1,20 @@
+function guessGifts(wishlist, presents) {
+    
+var ar = [];
+for(var i in wishlist){
+    
+    for(var j in presents){
+        
+        if(wishlist[i].size===presents[j].size && wishlist[i].clatters===presents[j].clatters && wishlist[i].weight===presents[j].weight){
+            
+            ar.push(wishlist[i].name);
+        }
+    }
+    
+}
+var array = ar.filter(function(elem, pos) {
+return ar.indexOf(elem) == pos;
+  }); 
+
+return array;
+}
